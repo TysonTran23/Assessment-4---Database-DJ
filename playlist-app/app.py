82876230beecb74549bc6dcefeb5ed5deca52bd0
Tyson Tran <tysontran@Tysons-MBP.hitronhub.home>
2023-07-17 19:27:11 -0700

    finished routes

--- playlist-app/app.py
+++ playlist-app/app.py
@@ -1,18 +1,18 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, flash, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db, Playlist, Song, PlaylistSong
-from forms import NewSongForPlaylistForm, SongForm, PlaylistForm
+from forms import NewSongForPlaylistForm, PlaylistForm, SongForm
+from models import Playlist, PlaylistSong, Song, connect_db, db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///playlist-app'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///playlist-app"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 
 connect_db(app)
 db.create_all()
 
-app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+app.config["SECRET_KEY"] = "I'LL NEVER TELL!!"
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -46,17 +46,32 @@ def show_playlist(playlist_id):
     """Show detail on specific playlist."""
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    playlist = Playlist.query.get(playlist_id)
+
+    return render_template("playlist.html", playlist=playlist)
 
 
 @app.route("/playlists/add", methods=["GET", "POST"])
 def add_playlist():
     """Handle add-playlist form:
-
     - if form not filled out or invalid: show form
     - if valid: add playlist to SQLA and redirect to list-of-playlists
     """
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    form = PlaylistForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+
+        new_playlist = Playlist(name=name, description=description)
+
+        db.session.add(new_playlist)
+        db.session.commit()
+
+        return redirect("/")
+    else:
+        return render_template("new_playlist.html", form=form)
 
 
 ##############################################################################
@@ -76,6 +91,8 @@ def show_song(song_id):
     """return a specific song"""
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    song = Song.query.get(song_id)
+    return render_template("song.html", song=song)
 
 
 @app.route("/songs/add", methods=["GET", "POST"])
@@ -87,6 +104,20 @@ def add_song():
     """
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    form = SongForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        artist = form.artist.data
+
+        new_song = Song(title=title, artist=artist)
+
+        db.session.add(new_song)
+        db.session.commit()
+
+        return redirect("/songs")
+    else:
+        return render_template("new_song.html", form=form)
 
 
 @app.route("/playlists/<int:playlist_id>/add-song", methods=["GET", "POST"])
@@ -106,11 +137,8 @@ def add_song_to_playlist(playlist_id):
     form.song.choices = ...
 
     if form.validate_on_submit():
+        # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
-          # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+        return redirect(f"/playlists/{playlist_id}")
 
-          return redirect(f"/playlists/{playlist_id}")
-
-    return render_template("add_song_to_playlist.html",
-                             playlist=playlist,
-                             form=form)
+    return render_template("add_song_to_playlist.html", playlist=playlist, form=form)
--- playlist-app/forms.py
+++ playlist-app/forms.py
@@ -1,20 +1,23 @@
 """Forms for playlist app."""
 
-from wtforms import SelectField
+from wtforms import SelectField, StringField
 from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired
 
 
 class PlaylistForm(FlaskForm):
     """Form for adding playlists."""
 
     # Add the necessary code to use this form
-
+    name = StringField("Name of Playlist", validators=[InputRequired(message="Playlist name can't be blank")])
+    description = StringField("Description of Playlist")
 
 class SongForm(FlaskForm):
     """Form for adding songs."""
 
     # Add the necessary code to use this form
-
+    title = StringField("Title", validators=[InputRequired(message="Name of song can't be blank")])
+    artist = StringField("Artist", validators=[InputRequired(message="Name of artist can't be blank")])
 
 # DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
 class NewSongForPlaylistForm(FlaskForm):
